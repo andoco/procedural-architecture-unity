065b6fe82aa94bc8c4357d26ebe5a3878ef29269
Andrew O'Connor <andrew.oconnor@gmail.com>
2014-07-19 13:49:23 +0100

    Depth first TreeNode traversal

--- Assets/TreeNodeTraversalExtensions.cs
+++ Assets/TreeNodeTraversalExtensions.cs
@@ -6,6 +6,8 @@ public static class TreeNodeTraversalExtensions
 {
 	public delegate void TreeNodeAction(TreeNode node);
 
+	public delegate void TreeNodeDepthFirstAction(TreeNode node, int depth);
+
 	public static void TraverseBreadthFirst(this TreeNode root, TreeNodeAction action)
 	{
 		var queue = new Queue<TreeNode>();
@@ -16,6 +18,21 @@ public static class TreeNodeTraversalExtensions
 			var current = queue.Dequeue();
 			current.ForEach(queue.Enqueue);
 			action(current);
+		}
+	}
+
+	public static void TraverseDepthFirst(this TreeNode root, TreeNodeDepthFirstAction action)
+	{
+		TraverseDepthFirst(root, 0, action);
+	}
+
+	private static void TraverseDepthFirst(this TreeNode root, int depth, TreeNodeDepthFirstAction action)
+	{
+		action(root, depth);
+		
+		foreach (var child in root)
+		{
+			TraverseDepthFirst(child, depth + 1, action);
 		}
 	}
 }
