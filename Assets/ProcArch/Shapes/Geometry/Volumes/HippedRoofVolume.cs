2efd618bf82e179d3ab2c19c1649fbdc44896ccc
Andrew O'Connor <andrew.oconnor@gmail.com>
2014-08-02 15:10:56 +0100

    Set y-axis of face scopes to zero for all volumes

--- Assets/ProcArch/Shapes/Geometry/Volumes/HippedRoofVolume.cs
+++ Assets/ProcArch/Shapes/Geometry/Volumes/HippedRoofVolume.cs
@@ -42,10 +42,10 @@ public class HippedRoofVolume : Volume
 		this.Faces.Add(new Face("face-end-1", new List<Corner> { this.Corners[0], this.Corners[4], this.Corners[3] }));
 		this.Faces.Add(new Face("face-end-2", new List<Corner> { this.Corners[1], this.Corners[2], this.Corners[5] }));
 
-		this.Components.Add(new ScopeComponent(this.Faces[0].Name, new SimpleTransform(new Vector3(0.25f, 0.5f, 0f), Quaternion.AngleAxis(45f, Vector3.forward), Vector3.one), v => v));
-		this.Components.Add(new ScopeComponent(this.Faces[1].Name, new SimpleTransform(new Vector3(-0.25f, 0.5f, 0f), Quaternion.AngleAxis(-45, Vector3.forward), Vector3.one), v => v));
-		this.Components.Add(new ScopeComponent(this.Faces[2].Name, new SimpleTransform(new Vector3(-0.5f, 0.5f, 0f), Quaternion.LookRotation(Vector3.forward, Vector3.up), Vector3.one), v => v.ToZXY()));
-		this.Components.Add(new ScopeComponent(this.Faces[3].Name, new SimpleTransform(new Vector3(0f, 0.5f, 0.5f), Quaternion.LookRotation(Vector3.back, Vector3.up), Vector3.one), v => v.ToZXY()));
+		this.Components.Add(new ScopeComponent(this.Faces[0].Name, new SimpleTransform(new Vector3(0.25f, 0.5f, 0f), Quaternion.AngleAxis(45f, Vector3.forward), new Vector3(1f, 0f, 1f)), v => v));
+		this.Components.Add(new ScopeComponent(this.Faces[1].Name, new SimpleTransform(new Vector3(-0.25f, 0.5f, 0f), Quaternion.AngleAxis(-45, Vector3.forward), new Vector3(1f, 0f, 1f)), v => v));
+		this.Components.Add(new ScopeComponent(this.Faces[2].Name, new SimpleTransform(new Vector3(-0.5f, 0.5f, 0f), Quaternion.LookRotation(Vector3.forward, Vector3.up), new Vector3(1f, 0f, 1f)), v => v.ToZXY()));
+		this.Components.Add(new ScopeComponent(this.Faces[3].Name, new SimpleTransform(new Vector3(0f, 0.5f, 0.5f), Quaternion.LookRotation(Vector3.back, Vector3.up), new Vector3(1f, 0f, 1f)), v => v.ToZXY()));
 	}
 
 	public override void ApplyStyle (IStyleConfig styleConfig)
