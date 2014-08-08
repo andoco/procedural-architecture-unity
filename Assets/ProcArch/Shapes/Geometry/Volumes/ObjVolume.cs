e889ba731301bccef8cbb7e22c5531973f6047fa
Andrew O'Connor <andrew.oconnor@gmail.com>
2014-08-09 00:40:48 +0100

    Caching ObjVolume data for multiple instances

--- Assets/ProcArch/Shapes/Geometry/Volumes/ObjVolume.cs
+++ Assets/ProcArch/Shapes/Geometry/Volumes/ObjVolume.cs
@@ -1,45 +1,65 @@
-using UnityEngine;
 using System.Collections.Generic;
-using ObjLoader.Loader.Loaders;
+using System.Globalization;
 using System.IO;
 using System.Text;
-using System.Globalization;
-using Andoco.Unity.Framework.Core.Meshes;
+using ObjLoader.Loader.Loaders;
+using UnityEngine;
 
 public class ObjVolume : Volume
 {
-	private LoadResult result;
+	private static readonly Dictionary<string, ObjVolumeCacheItem> objVolCache = new Dictionary<string, ObjVolumeCacheItem>();
 
 	protected override void OnBuildVolume(Argument[] args)
 	{
 		var objFile = args.GetArgOrDefault<string>("objFile", null);
 
+		ObjVolumeCacheItem cacheItem;
+
+		if (objVolCache.TryGetValue(objFile, out cacheItem))
+		{
+			RestoreFromCacheItem(cacheItem);
+		}
+		else
+		{
+			var result = LoadObj(objFile);
+			BuildFromObj(result);
+			StoreInCache(objFile);
+		}
+	}
+
+	private LoadResult LoadObj(string objFile)
+	{
 		var objLoaderFactory = new ObjLoaderFactory();
 		var objLoader = objLoaderFactory.Create();
 		
 		var text = (TextAsset)Resources.Load(objFile);
 		var ms = new MemoryStream(Encoding.UTF8.GetBytes(text.text));
-		this.result = objLoader.Load(ms);
+		var result = objLoader.Load(ms);
 
+		return result;
+	}
+
+	private void BuildFromObj(LoadResult result)
+	{
 		var cornerIndex = new Dictionary<Vector3, Corner>();
-
-		for (var i=0; i < this.result.Vertices.Count; i++)
+		
+		for (var i=0; i < result.Vertices.Count; i++)
 		{
-			var v = this.result.Vertices[i];
+			var v = result.Vertices[i];
 			var c = new Corner(string.Format("corner-{0}", i), new Vector3(v.X, v.Y, v.Z));
 			this.Corners.Add(c);
 			cornerIndex[c.Position] = c;
 		}
-
-		for (var i=0; i < this.result.Groups.Count; i++)
+		
+		for (var i=0; i < result.Groups.Count; i++)
 		{
-			var group = this.result.Groups[i];
-
+			var group = result.Groups[i];
+			
 			for (var j=0; j < group.Faces.Count; j++)
 			{
 				var face = group.Faces[j];
 				var faceCorners = new Corner[face.Count];
-
+				
 				for (var k=0; k < face.Count; k++)
 				{
 					var fv = face[k];
@@ -47,9 +67,42 @@ public class ObjVolume : Volume
 					var fc = cornerIndex[new Vector3(v.X, v.Y, v.Z)];
 					faceCorners[k] = fc;
 				}
-
+				
 				this.Faces.Add(new Face(string.Format("face-{0}-{1}", j, i), faceCorners));
 			}
 		}
+	}
+
+	private void StoreInCache(string objFile)
+	{
+		var cacheItem = new ObjVolumeCacheItem
+		{
+			Corners = this.Corners,
+			Edges = this.Edges,
+			Faces = this.Faces,
+			Components = this.Components
+		};
+
+		objVolCache[objFile] = cacheItem;
+	}
+
+	private void RestoreFromCacheItem(ObjVolumeCacheItem cacheItem)
+	{
+		foreach (var c in cacheItem.Corners)
+			this.Corners.Add(c);
+		foreach (var e in cacheItem.Edges)
+			this.Edges.Add(e);
+		foreach (var f in cacheItem.Faces)
+			this.Faces.Add(f);
+		foreach (var c in cacheItem.Components)
+			this.Components.Add(c);
+	}
+
+	private class ObjVolumeCacheItem
+	{
+		public IList<Corner> Corners { get; set; }
+		public IList<Edge> Edges { get; set; }
+		public IList<Face> Faces { get; set; }
+		public IList<ScopeComponent> Components { get; set; }
 	}
 }
