79dc0a65cc36f76112fbbfa1f5d067ca940426f7
Andrew O'Connor <andrew.oconnor@gmail.com>
2014-08-09 01:16:27 +0100

    Testing some more colors

--- Assets/ProcArch/CommonArchitectureStyleConfig.cs
+++ Assets/ProcArch/CommonArchitectureStyleConfig.cs
@@ -17,14 +17,20 @@ public class CommonArchitectureStyleConfig : StyleConfig
 		var lightWood = new Color(124f/255f, 109f/255f, 94f/255f);
 		var glass = new Color(206f/255f, 217f/255f, 203f/255f);
 
+		var windowLit = new Color(255f/255f, 255f/255f, 200f/255f);
+
 		var beige = new Color(208f/255f, 197f/255f, 133f/255f);
 		var grey = new Color(110f/255f, 110f/255f, 110f/255f);
 		var lightRed = new Color(255f/255f, 195f/255f, 0);
 
-		var roofTop = darkWood;
-		var facade = lightWood;
-		var window = glass;
-		
+		var godusFacade = new Color(194f/255f, 184f/255f, 139f/255f);
+		var godusRedRoof = new Color(196f/255f, 82f/255f, 48f/255f);
+		var godusBlueRoof = new Color(46f/255f, 128f/255f, 194f/255f);
+
+		var roofTop = godusRedRoof;
+		var facade = godusFacade;
+		var window = windowLit;
+
 		var styles = new Dictionary<string, IDictionary<string, object>> {
 			{ "default", new Dictionary<string, object> { 
 					{ "face-color", grey }
