53d6c6eab4767282da6003ca6fa09565986c77d9
Andrew O'Connor <andrew.oconnor@gmail.com>
2014-08-09 23:19:30 +0100

    Script to export ProcArch package

---
+++ export-package.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -projectPath ~/Dropbox/Code/Unity/ProceduralArchitecture -exportPackage Assets/ProcArch ~/Desktop/ProcArch.unitypackage -quit
