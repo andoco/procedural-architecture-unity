d7db727a7b2be35249a7b24c8a89c0470f4b55dc
Andrew O'Connor <andrew.oconnor@gmail.com>
2014-08-17 00:56:06 +0100

    Silo source. Added to MultipleHouses scene.

--- Assets/ProcArch/Shapes/Geometry/Volumes/ConeVolume.cs
+++ Assets/ProcArch/Shapes/Geometry/Volumes/ConeVolume.cs
@@ -25,30 +25,28 @@ namespace Andoco.Unity.ProcArch.Shapes.Geometry.Volumes
             var angleDelta = Mathf.PI * 2f / this.NumSegments;
             var segWidth = Mathf.Sin (angleDelta / 2f);
     
-            var bottomCorner = new Corner ("corner-bottom", new Vector3 (0f, 0f, 0f));
-            this.Corners.Add (bottomCorner);
+            var bottomCorner = this.AddCorner("corner-bottom", new Vector3 (0f, 0f, 0f));
             
-            var topCorner = new Corner ("corner-top", new Vector3 (0f, 1f, 0f));
-            this.Corners.Add (topCorner);
+            var topCorner = this.AddCorner("corner-top", new Vector3 (0f, 1f, 0f));
     
             for (int i=0; i <= this.NumSegments; i++) {
                 var a = angleDelta * -i;
     
                 var p1 = new Vector3 (Mathf.Cos (a) * 0.5f, 0f, Mathf.Sin (a) * 0.5f);
                 
-                this.Corners.Add (new Corner (string.Format ("corner-bottom-{0}", i), p1));
+                this.AddCorner(string.Format ("corner-bottom-{0}", i), p1);
     
                 var numCorners = this.Corners.Count;
                 
                 if (i > 0) {
-                    this.Edges.Add (new Edge (string.Format ("edge-bottom-{0}", i), this.Corners [numCorners - 2], this.Corners [numCorners - 1]));
+                    this.AddEdge(string.Format ("edge-bottom-{0}", i), this.Corners[numCorners - 2], this.Corners[numCorners - 1]);
                 }
                 
-                this.Edges.Add (new Edge (string.Format ("edge-side-{0}", i), this.Corners [numCorners - 2], this.Corners [numCorners - 1]));
+                this.AddEdge(string.Format ("edge-side-{0}", i), this.Corners[numCorners - 2], this.Corners[numCorners - 1]);
     
                 if (i > 0) {
-                    this.Faces.Add (new Face (string.Format ("face-side-{0}", i), new List<Corner> { this.Corners[numCorners - 2], this.Corners[numCorners - 1], topCorner }));
-                    this.Faces.Add (new Face (string.Format ("face-bottom-{0}", i), new List<Corner> { this.Corners[numCorners - 2], bottomCorner, this.Corners[numCorners - 1] }));
+                    this.AddFace(string.Format ("face-side-{0}", i), this.Corners[numCorners - 2], this.Corners[numCorners - 1], topCorner);
+                    this.AddFace(string.Format ("face-bottom-{0}", i), this.Corners[numCorners - 2], bottomCorner, this.Corners[numCorners - 1]);
                 }
             }
     
@@ -56,13 +54,12 @@ namespace Andoco.Unity.ProcArch.Shapes.Geometry.Volumes
                 var upDir = face.GetCentre ();
                 upDir.y = 0;
                 
-                this.Components.Add (
-                    new ScopeComponent (
+                this.AddComponent(
                     face.Name,
-                    new SimpleTransform (face.GetCentre (), Quaternion.LookRotation (Vector3.up, upDir), new Vector3 (segWidth, 0f, 1f)), x => x.ToZXY ()));
+                    new SimpleTransform (face.GetCentre (), Quaternion.LookRotation (Vector3.up, upDir), new Vector3 (segWidth, 0f, 1f)), x => x.ToZXY());
             }
     
-            this.Components.Add (new ScopeComponent ("face-bottom", new SimpleTransform (new Vector3 (0f, 0f, 0f), Quaternion.LookRotation (Vector3.forward, Vector3.down), new Vector3(1f, 0f, 1f)), x => x));
+            this.AddComponent("face-bottom", new SimpleTransform (new Vector3 (0f, 0f, 0f), Quaternion.LookRotation (Vector3.forward, Vector3.down), new Vector3(1f, 0f, 1f)), x => x);
         }
         
         protected override void ApplyStyle(IStyleConfig styleConfig)
--- Assets/ProcArch/Shapes/Geometry/Volumes/CylinderVolume.cs
+++ Assets/ProcArch/Shapes/Geometry/Volumes/CylinderVolume.cs
@@ -25,11 +25,8 @@ namespace Andoco.Unity.ProcArch.Shapes.Geometry.Volumes
             var angleDelta = Mathf.PI * 2f / this.NumSegments;
             var segWidth = Mathf.Sin (angleDelta / 2f);
     
-            var bottomCorner = new Corner ("corner-bottom", new Vector3 (0f, 0f, 0f));
-            this.Corners.Add (bottomCorner);
-            
-            var topCorner = new Corner ("corner-top", new Vector3 (0f, 1f, 0f));
-            this.Corners.Add (topCorner);
+            var bottomCorner = this.AddCorner("corner-bottom", new Vector3(0f, 0f, 0f));
+            var topCorner = this.AddCorner("corner-top", new Vector3 (0f, 1f, 0f));
     
             for (int i=0; i <= this.NumSegments; i++) {
                 var a = angleDelta * -i;
@@ -37,22 +34,22 @@ namespace Andoco.Unity.ProcArch.Shapes.Geometry.Volumes
                 var p1 = new Vector3 (Mathf.Cos (a) * 0.5f, 0f, Mathf.Sin (a) * 0.5f);
                 var p2 = new Vector3 (Mathf.Cos (a) * 0.5f, 1f, Mathf.Sin (a) * 0.5f);
                 
-                this.Corners.Add (new Corner (string.Format ("corner-bottom-{0}", i), p1));
-                this.Corners.Add (new Corner (string.Format ("corner-top-{0}", i), p2));
+                this.AddCorner(string.Format ("corner-bottom-{0}", i), p1);
+                this.AddCorner(string.Format ("corner-top-{0}", i), p2);
     
                 var numCorners = this.Corners.Count;
                 
                 if (i > 0) {
-                    this.Edges.Add (new Edge (string.Format ("edge-bottom-{0}", i), this.Corners [this.Corners.Count - 2], this.Corners [this.Corners.Count - 4]));
-                    this.Edges.Add (new Edge (string.Format ("edge-top-{0}", i), this.Corners [this.Corners.Count - 1], this.Corners [this.Corners.Count - 3]));
+                    this.AddEdge(string.Format("edge-bottom-{0}", i), this.Corners.Count - 2, this.Corners.Count - 4);
+                    this.AddEdge(string.Format("edge-top-{0}", i), this.Corners.Count - 1, this.Corners.Count - 3);
                 }
                 
-                this.Edges.Add (new Edge (string.Format ("edge-side-{0}", i), this.Corners [this.Corners.Count - 2], this.Corners [this.Corners.Count - 1]));
+                this.AddEdge(string.Format("edge-side-{0}", i), this.Corners.Count - 2, this.Corners.Count - 1);
     
                 if (i > 0) {
-                    this.Faces.Add (new Face (string.Format ("face-side-{0}", i), new List<Corner> { this.Corners[numCorners - 4], this.Corners[numCorners - 2], this.Corners[numCorners - 1], this.Corners[numCorners - 3] }));
-                    this.Faces.Add (new Face (string.Format ("face-bottom-{0}", i), new List<Corner> { this.Corners[numCorners - 4], bottomCorner, this.Corners[numCorners - 2] }));
-                    this.Faces.Add (new Face (string.Format ("face-top-{0}", i), new List<Corner> { this.Corners[numCorners - 3], this.Corners[numCorners - 1], topCorner }));
+                    this.AddFace(string.Format("face-side-{0}", i), numCorners - 4, numCorners - 2, numCorners - 1, numCorners - 3);
+                    this.AddFace(string.Format ("face-bottom-{0}", i), this.Corners[numCorners - 4], bottomCorner, this.Corners[numCorners - 2]);
+                    this.AddFace(string.Format ("face-top-{0}", i), this.Corners[numCorners - 3], this.Corners[numCorners - 1], topCorner);
                 }
             }
     
@@ -60,15 +57,13 @@ namespace Andoco.Unity.ProcArch.Shapes.Geometry.Volumes
                 var upDir = face.GetCentre ();
                 upDir.y = 0;
                 
-                this.Components.Add (
-                    new ScopeComponent (
+                this.AddComponent(
                     face.Name,
-                    new SimpleTransform (face.GetCentre (), Quaternion.LookRotation (Vector3.up, upDir), new Vector3 (segWidth, 0f, 1f)), x => x.ToZXY ()));
+                    new SimpleTransform (face.GetCentre (), Quaternion.LookRotation (Vector3.up, upDir), new Vector3 (segWidth, 0f, 1f)), x => x.ToZXY());
             }
     
-            this.Components.Add (new ScopeComponent ("face-top", new SimpleTransform (new Vector3 (0f, 1f, 0f), Quaternion.LookRotation (Vector3.forward, Vector3.up), new Vector3 (1f, 0f, 1f)), x => x));
-            this.Components.Add (new ScopeComponent ("face-bottom", new SimpleTransform (new Vector3 (0f, 0f, 0f), Quaternion.LookRotation(Vector3.forward, Vector3.down), new Vector3(1f, 0f, 1f)), x => x));
-            
+            this.AddComponent("face-top", new SimpleTransform (new Vector3 (0f, 1f, 0f), Quaternion.LookRotation (Vector3.forward, Vector3.up), new Vector3 (1f, 0f, 1f)), x => x);
+            this.AddComponent("face-bottom", new SimpleTransform (new Vector3 (0f, 0f, 0f), Quaternion.LookRotation(Vector3.forward, Vector3.down), new Vector3(1f, 0f, 1f)), x => x);
         }
         
         protected override void ApplyStyle (IStyleConfig styleConfig)
