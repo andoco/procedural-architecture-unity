33a52389de6cfd2eb4fea3cf80afea8aa67c5d6f
Andrew O'Connor <andrew.oconnor@gmail.com>
2015-03-01 14:54:39 +0000

    Simple castle demo

---
+++ Assets/Demos/DemoCameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoCameraController : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.Rotate(45f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.Rotate(-45f);
+        }
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.Zoom(1f);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.Zoom(-1f);
+        }
+    }
+
+    private void Rotate(float amount)
+    {
+        Camera.main.transform.RotateAround(Vector3.zero, Vector3.up, amount * Time.deltaTime);
+    }
+    
+    private void Zoom(float amount)
+    {
+        Camera.main.transform.Translate(Vector3.forward * amount, Space.Self);
+    }
+}
